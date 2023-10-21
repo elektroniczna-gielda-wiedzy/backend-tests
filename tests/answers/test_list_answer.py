c21dfda639965a8b2be674d3b95b427ba9dcf0a1
mie378 <pkociuba@student.agh.edu.pl>
2023-10-21 03:42:02 +0200

    add tests for standard answer sorting

--- tests/answers/test_list_answer.py
+++ tests/answers/test_list_answer.py
@@ -19,3 +19,27 @@ def test_list_answers(auth_token_user1, app_url, create_temporary_entry):
 
     # cleanup
     cleanup()
+
+
+def test_answer_default_sorting(auth_token_user1, auth_token_user2, auth_token_user3, app_url, create_temporary_entry):
+    # setup
+    entry_id, cleanup = create_temporary_entry(auth_token=auth_token_user1)
+    service = AnswerService(auth_token_user1, app_url)
+    service2 = AnswerService(auth_token_user2, app_url)
+    service3 = AnswerService(auth_token_user3, app_url)
+
+    # test
+    service.add(entry_id, "test content 1", None)
+    service2.add(entry_id, "test content 2", None)
+    service3.add(entry_id, "test content 3", None)
+    answers = service.list(entry_id).json()['result']
+
+    # assert
+    assert answers[0]['content'] == 'test content 1'
+    assert answers[1]['content'] == 'test content 2'
+    assert answers[2]['content'] == 'test content 3'
+
+    # cleanup
+    cleanup()
+
+
